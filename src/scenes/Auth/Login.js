739ad65323f91c30f4f7cb99b3e1601e28f8ac9f
Rizki Fr <rizqifauzi12@gmail.com>
2020-02-20 23:12:39 +0700

    implementation api login

--- src/scenes/Auth/Login.js
+++ src/scenes/Auth/Login.js
@@ -1,28 +1,55 @@
 import React from 'react';
-import { View, ImageBackground, Dimensions, Image, StyleSheet, StatusBar } from 'react-native';
+import { View, ImageBackground, Dimensions, Image, StyleSheet, StatusBar, Alert } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { saveUser } from '_states/actions/user';
 import { connect } from 'react-redux';
 import { Button, Input } from '_atoms'
 import { color } from '_styles';
+import Axios from 'axios';
 
 class Login extends React.Component {
+    constructor(props){
+        super(props),
+        this.state={
+            user:{
+                username: '',
+                password: ''
+            },
+            loading: false
+        }
+    }
+
+    handleChange = async (type, val) => {
+        const user = Object.assign({}, this.state.user)
+        user[type] = val
+        this.setState({user})
+    }
 
     login = async () => {
-        await AsyncStorage.setItem('auth', 'true')
-        this.props.dispatch(saveUser({ name: 'Rizki Fr' }))
-        this.props.navigation.navigate('MiddleWare')
+        this.setState({loading: true})
+        const user = this.state.user
+        Axios.post('/auth/login', user)
+            .finally(()=>this.setState({loading: false}))
+            .then(res=>{
+                AsyncStorage.setItem('token', res.data.access_token)
+                this.props.dispatch(saveUser(res.data.user))
+                this.props.navigation.navigate('MiddleWare')
+            }).catch(err=>{
+                Alert.alert('Error', err.message)
+            })
     }
 
     render() {
+        const { user, loading } = this.state
+        const disable = !user.username || user.password.length<6
         return (
             <ImageBackground source={require('_assets/images/bg.jpg')} style={{height: height}} resizeMode='cover'>
                 <StatusBar hidden />
                 <View style={styles.wraper}>
                     <Image source={require('_assets/images/logo.png')} style={styles.logo} resizeMode='contain' />
-                    <Input placeholder='Email' icon='user' />
-                    <Input placeholder='Password' icon='eye-slash' password />
-                    <Button title='LOGIN' onPress={this.login} style={{marginTop: 20}} />
+                    <Input placeholder='Email' icon='user' onChangeText={(val)=>this.handleChange('username', val)} />
+                    <Input placeholder='Password' icon='eye-slash' password onChangeText={(val)=>this.handleChange('password', val)} />
+                    <Button title='LOGIN' loading={loading} disabled={loading || disable} onPress={this.login} style={{marginTop: 20}} />
                 </View>
             </ImageBackground>
         )
