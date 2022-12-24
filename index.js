f70583711c78a88c1caba480dbb6f6472d0a6265
okireval <okijulianda12@gmail.com>
2022-12-24 15:59:48 +0700

    init project for new version

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src';
+import App from './App';
 import {name as appName} from './app.json';
-import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
+AppRegistry.registerComponent(appName, () => App);
